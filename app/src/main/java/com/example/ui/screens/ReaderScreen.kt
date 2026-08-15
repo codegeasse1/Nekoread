be53105b8da1be852e745088bddadacd57c6670b
codegeasse <chattered199@gmail.com>
2026-08-15 21:55:02 +0530

    ReaderScreen: import mutableStateListOf (fix build)

--- app/src/main/java/com/example/ui/screens/ReaderScreen.kt
+++ app/src/main/java/com/example/ui/screens/ReaderScreen.kt
@@ -61,6 +61,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
